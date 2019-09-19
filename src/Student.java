23889219f6493f4dc1b93702927c167099534b4f
macstudent <komaldeepkr99@gmail.com>
2019-09-18 20:40:13 -0400

    STUDENT ID,NAME

--- src/Student.java
+++ src/Student.java
@@ -1,7 +1,18 @@
-package com.lambton
+package com.lambton;
 
 public class Student
 {
     int studentID;
     String studentName;
+    void setvalues()
+    {
+        studentID=1;
+        studentName="Komal";
+    }
+
+    void display()
+    {
+        System.out.println("Student ID :" + studentID);
+        System.out.println("Student Name :"+ studentName);
+    }
 }
--- src/com/lambton/FirstDay.java
+++ src/com/lambton/FirstDay.java
@@ -9,6 +9,17 @@ public class FirstDay
         a=10;
         b=20;
         c=a+b;
-        System.out.println("Sum:" +c);
+        System.out.println("Sum:" + c);
+
+        int x= Integer.MAX_VALUE;
+        System.out.println("X :" + x);
+
+        int y=x+1;
+        System.out.println("Y :" + y);
+        System.out.println("Min : " +Integer.MIN_VALUE);
+
+        Student s1 = new Student();
+        s1.setvalues();
+        s1.display();
     }
 }
